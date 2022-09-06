f3cdbb55c9e41235dcda9e0134d3f29375cb9c85
karan0623 <64561330+karan0623@users.noreply.github.com>
2022-09-06 14:53:51 -0400

    Add Javadoc to / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -15,6 +15,12 @@ public class PlantPlacesController {
 	public String start() {
 		return "start";
 	}
+	
+	/**
+	 * Handle the / endpoint
+	 * return
+	 *
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "start";
