87ca976975f11c74a4606c10e6317b0eced7d311
karan0623 <64561330+karan0623@users.noreply.github.com>
2022-09-22 15:09:23 -0400

    Environment Set up DONE!

---
+++ src/main/java/com/plantplaces/service/ISpecimenService.java
@@ -0,0 +1,32 @@
+package com.plantplaces.service;
+
+import com.plantplaces.dto.SpecimenDTO;
+
+
+
+
+/**
+ * CRUD operations for specimen
+ *
+ */
+
+public interface ISpecimenService {
+	
+	/**
+	 * Get specimens from persistence layer
+	 * @param id a unique lookup
+	 * @return a specimen with a matching id
+	 *
+	 */
+
+	SpecimenDTO fetchById(int id);
+	
+	/**
+	 * persist the given DTO
+	 * @param specimenDTO
+	 *
+	 */
+
+	void save(SpecimenDTO specimenDTO);
+
+}
